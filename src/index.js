82332ab7ba7025b01f14033a2cb107f55ee10e14
Mike Duin <michael.s.duin@gmail.com>
2018-12-02 23:42:07 -0800

    reconfigure function component to class component, initialize state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    // This callback above is referred to as the 'success callback' as it gets called every time everything goes as planned
-    (err) => console.log(err)
-    // This is the error callback if something breaks
-  );
+class App extends React.Component {
 
-  return (
-    <div> Hi there! </div>
-  )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null
+
+    };
+  }
+
+
+  // React says we have to define render!! This is a requiremement in every React component
+  render () {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      // This callback above is referred to as the 'success callback' as it gets called every time everything goes as planned
+      (err) => console.log(err)
+      // This is the error callback if something breaks
+    );
+
+    return (
+      <div> Latitude: </div>
+    )
+  }
 }
 
 ReactDOM.render(
