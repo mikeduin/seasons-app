8291d3bb702d6e02a6d87ec142e3ad1f38c37a95
Mike Duin <michael.s.duin@gmail.com>
2018-12-03 23:55:54 -0800

    add semantic ui icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,11 +10,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-
-  console.log(season);
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return (
-    <div> Latitude: {props.lat} </div>
+    <div>
+      <i className={`${icon} icon`} />
+      <h1> { text } </h1>
+      <i className={`${icon} icon`} />
+    </div>
   )
 };
 
