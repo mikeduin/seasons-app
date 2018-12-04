bceb507a446bf7f2c0a0a8ff96e7c5f29d09ca25
Mike Duin <michael.s.duin@gmail.com>
2018-12-03 23:33:36 -0800

    incorporate SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
-  }
+  state = { lat: null, errorMessage: ''};
 
   componentDidMount () {
     window.navigator.geolocation.getCurrentPosition(
@@ -25,7 +19,7 @@ class App extends React.Component {
       return <div> Error: {this.state.errorMessage} </div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude: {this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
     return <div> Loading! </div>
   }
